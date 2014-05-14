106f8708e5af0b1d7d9e182f769d35be8c175394
Konrad Kosiorek <konrad.kosiorek@gmail.com>
2014-05-14 12:32:06 +0200

    touch handling improvement
    changed touch event handling from buttons to touchEvents

--- Source/Gameplay.m
+++ Source/Gameplay.m
@@ -16,38 +16,55 @@
     CCNode *_cloud3;
     CCNode *_cloud4;
     CCNode *_cloud5;
+    CCNode *_btnUp;
+    CCNode *_btnDown;
+    CCNode *_btnLeft;
+    CCNode *_btnRight;
     CCNode *_axe;
     CCNode *_ground;
+    CCLabelTTF *_scoreLabel;
     NSArray *_cloudsSlow;
     NSArray *_cloudsFast;
     NSArray *_allClouds;
     CCPhysicsNode *_physicsNode;
     CCNode *_clouds;
-    int screenWidth;
+    NSInteger _screenWidth;
+    NSInteger gamePoints;
     BOOL isThrowing;
     BOOL isJumping;
     OALSimpleAudio *audio;
+    
 }
+
+-(id)init{
+    if(self = [super init]){
+        self.userInteractionEnabled = TRUE;
+        audio = [OALSimpleAudio sharedInstance];
+        [audio preloadEffect:@"hatchet.mp3"];
+        gamePoints = 0;
+        isThrowing = FALSE;
+        isJumping = FALSE;
+    }
+    return self;
+}
+
 -(BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair *)pair hero:(CCNode *)_hero ground:(CCNode *)_ground{
+    CCLOG(@"landing");
     isJumping = FALSE;
     return TRUE;
 }
 -(void)didLoadFromCCB{
-    audio = [OALSimpleAudio sharedInstance];
-    [audio preloadEffect:@"hatchet.mp3"];
-    isThrowing = FALSE;
-    isJumping = FALSE;
-    _physicsNode.collisionDelegate = self;
     _cloudsSlow =@[_cloud1, _cloud2];
     _cloudsFast =@[_cloud3, _cloud4, _cloud5];
     _allClouds = @[_cloud1, _cloud2,_cloud3, _cloud4, _cloud5];
-    screenWidth = _cloud1.contentSize.width;
+    _screenWidth = _cloud1.contentSize.width;
+    _physicsNode.collisionDelegate = self;
     [_hero.physicsBody setCollisionType:@"hero"];
     [_ground.physicsBody setCollisionType:@"ground"];
 }
 
 -(void)update:(CCTime)delta{
-    if(_axe.position.x>screenWidth){
+    if(_axe.position.x>_screenWidth){
         isThrowing = FALSE;
         [_axe removeFromParentAndCleanup:TRUE];
         _axe = nil;
@@ -62,22 +79,25 @@
         CGPoint cloudWorldPosition = [_clouds convertToWorldSpace:cloud.position];
         CGPoint cloudScreenPosition = [self convertToNodeSpace:cloudWorldPosition];
         if(cloudScreenPosition.x <= (-1*cloud.contentSize.width)){
-            cloud.position = ccp(cloud.position.x+cloud.contentSize.width+screenWidth, cloud.position.y);
+            cloud.position = ccp(cloud.position.x+cloud.contentSize.width+_screenWidth, cloud.position.y);
         }
     }
 }
 
--(void)btnUp{
-    [self jump];
-}
--(void)btnDown{
-    CCLOG(@"unik");
-}
--(void)btnRight{
-    [self launchAxe];
-}
--(void)btnLeft{
-    CCLOG(@"tarcza");
+-(void) touchBegan: (UITouch *)touch withEvent:(UIEvent *)event{
+    CGPoint touchLocation = [touch locationInNode: self];
+    if (CGRectContainsPoint([_btnUp boundingBox], touchLocation)){
+        [self jump];
+    }
+    if (CGRectContainsPoint([_btnRight boundingBox], touchLocation)){
+        [self launchAxe];
+    }
+    if (CGRectContainsPoint([_btnLeft boundingBox], touchLocation)){
+        CCLOG(@"tarcza");
+    }
+    if (CGRectContainsPoint([_btnDown boundingBox], touchLocation)){
+        CCLOG(@"unik");
+    }
 }
 
 -(void) jump{
@@ -86,6 +106,8 @@
 }
 -(void) launchAxe{
     if(!isThrowing && !isJumping){
+        gamePoints++;
+        _scoreLabel.string = [NSString stringWithFormat:@"%ld", (long)gamePoints];
         isThrowing = TRUE;
         _axe = [CCBReader load:@"axe"];
         _axe.position = ccpAdd(_hero.position, ccp(10,5));
